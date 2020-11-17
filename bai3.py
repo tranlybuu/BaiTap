b2c16126791f4e5c82807be2996f6d8ac8147d97
tranlybuu <lybuu9117@gmail.com>
2020-11-17 12:29:17 +0700

    Create bai3.py

---
+++ bai3.py
@@ -0,0 +1,7 @@
+n=int(input("Nhập n: "))
+print("=================")
+for i in range(0,n+1):            #Cho i chạy từ 0 -> n 
+    print(i)                      #với mỗi giá trị của i thì in ra màn hình
+print("=================")
+for i in range(0,n+1,2):          #Cho i chạy từ 0 -> n và step = 2
+    print(i)                      #với mỗi giá trị của i thì in ra màn hình
