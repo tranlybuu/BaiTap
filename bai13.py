dba0c3009f68e1827c49c7479798239a4709949e
tranlybuu <lybuu9117@gmail.com>
2020-11-29 22:19:24 +0700

    Create bai13.py

---
+++ bai13.py
@@ -0,0 +1,15 @@
+import string,random
+import random
+n=random.randint(50,1001)
+print("Số phần tử đã được random là số:",n)
+array=[]
+for i in range(n):
+    R=dict()
+    n=random.randrange(1,20)
+    G=random.choice(string.ascii_uppercase)
+    H=G+''.join(random.choice(string.ascii_lowercase) for i in range(n-1))
+    R['name']=H
+    A=random.randrange(1,100)
+    R['age']=A
+    array.append(R)
+print(array)
