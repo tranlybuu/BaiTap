6bdc98bba4d583c85b25e79ee4e5217f11bc7f9b
tranlybuu <lybuu9117@gmail.com>
2021-01-11 23:28:47 +0700

    Update web-s.py

--- web-s.py
+++ web-s.py
@@ -1,5 +1,28 @@
-import csv
+import requests
+from bs4 import BeautifulSoup
 
-with open('high_temps.csv','w') as f:
-    w = csv.writer(f)
-    w.writerows(data.items())
+page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.children)
+
+"""
+test=list(soup.children)
+for i in test:
+    print(" = ",i)
+print("=======")
+html = list(soup.children)[2]
+print(list(html.children))
+print("=======")
+body = list(html.children)[3]
+print(list(body.children))
+print("=======")
+p = list(html.children)[1]
+print(list(p.children))
+print(p.get_text())
+"""
+
+"""
+#print(soup.find_all('p'))
+print(soup.find_all('p')[0].get_text())                     #Tìm tất cả thẻ p
+print(soup.find('p'))                                       #Tìm thẻ p đầu tiên
+"""
